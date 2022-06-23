285277f55a718e53fa73a6b3e518057164daf1f0
theixi <bombalaigor@gmail.com>
2022-06-23 22:37:37 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ from flask_gravatar import Gravatar
 from flask_login import LoginManager
 from functools import wraps
 from flask import abort
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['FLASK_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
