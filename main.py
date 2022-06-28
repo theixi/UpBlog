2c3774b0bf204c454be1b61a80cbb6759f956331
Ixi <76916064+theixi@users.noreply.github.com>
2022-06-28 23:00:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
@@ -10,7 +10,6 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from flask_login import LoginManager
 from functools import wraps
-from flask import abort
 import os
 
 app = Flask(__name__)
